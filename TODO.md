2e3299fbbff8e2b1d68017b338c0e3e58aa6866c
Colby Birkhead <birkheadc@gmail.com>
2023-12-03 15:49:01 +0900

    create serverless (aws) configuration

--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@
 
 Set up auth: only admin should be able to update / create
 Set up pictures upload: something something buckets?
+Finish uploading my old projects
--- projects-portfolio-back/src/main.ts
+++ projects-portfolio-back/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe({enableDebugMessages: true}));
   app.enableCors();
   await app.listen(5000);
 }
